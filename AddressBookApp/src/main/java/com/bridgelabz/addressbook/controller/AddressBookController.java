29a66d38ca395102a74e7d56cd9e3f57f36f773b
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-29 00:14:28 +0530

    [Devesh]: created address book Controller

---
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.addressbook.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+	
+	@RequestMapping({"","/","/get"})
+	public ResponseEntity<String> getAddressBook() {
+		return new ResponseEntity<String>("Get call success",HttpStatus.OK);
+	}
+	
+	@GetMapping("/get/{contactid}")
+	public ResponseEntity<String> getAddressBookById(@PathVariable int contactid){
+		return new ResponseEntity<String>("Get by Id success",HttpStatus.OK);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<String> createAddressBook(){
+		return new ResponseEntity<String>("Created Successfully",HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<String> updateAddressBook(){
+		return new ResponseEntity<String>("Updated Successfully",HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<String> deleteAddressBook(){
+		return new ResponseEntity<String>("Deleted Successfully",HttpStatus.OK);
+	}
+	
+}
